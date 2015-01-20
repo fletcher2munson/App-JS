e1478e32916336848e6ea5aecca651646c4633a7
LuK <vfursikov@gmail.com>
2015-01-20 16:46:16 +0300

    0.1

---
+++ www/js/play_stop.js
@@ -0,0 +1,14 @@
+
+function play_stop_function(){
+    var my_media = new Media('http://cloudscanspeak.net:8000/clouds',
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        }
+    );
+    // Play audio
+    my_media.play();
+}
