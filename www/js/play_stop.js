9de9ec557c022e2548107be119540468ea6911e1
LuK <vfursikov@gmail.com>
2015-01-21 20:15:24 +0300

    0.1

---
+++ www/js/play_stop.js
@@ -0,0 +1,25 @@
+
+
+function play_stop_function(){
+
+    var my_media = new Media('http://cloudscanspeak.net:8000/clouds',
+        function () {
+            console.log("playAudio():Audio Success");
+        },
+        // error callback
+        function (err) {
+            console.log("playAudio():Audio Error: " + err);
+        },
+        function (stat) {
+            if (stat == 1) {
+                document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Buffering</div>';
+            }
+            if (stat == 2) {
+                document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Stop</div>';
+            }
+        }
+    );
+    // Play audio
+    my_media.play({playAudioWhenScreenIsLocked : false });
+}
+
