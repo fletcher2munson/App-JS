fbd3478aa40ad22cab5609dec0c15307ea0f3939
LuK <vfursikov@gmail.com>
2015-01-28 18:59:30 +0300

    0.1

--- www/js/play_stop.js
+++ www/js/play_stop.js
@@ -11,12 +11,26 @@ function load_media(){
             console.log("playAudio():Audio Error: " + err);
         },
         function (stat) {
-            stream_stat=stat;
             if (stat == 1) {
+                stream_stat=stat;
                 document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Buffering</div>';
+                document.getElementById("stream_status_monitor").innerHTML = "Starting";
+
             }
             if (stat == 2) {
+                stream_stat=stat;
                 document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Stop</div>';
+                document.getElementById("stream_status_monitor").innerHTML = "Running";
+            }
+            if (stat == 3) {
+                stream_stat=stat;
+                document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Paused</div>';
+                document.getElementById("stream_status_monitor").innerHTML = "Paused";
+            }
+            if (stat == 4) {
+                stream_stat=stat;
+                document.getElementById("play_stop_button").innerHTML = '<div class="app-button">Play</div>';
+                document.getElementById("stream_status_monitor").innerHTML = "Stopped";
             }
         }
     );
